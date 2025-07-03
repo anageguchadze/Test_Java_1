6e48f8045697bb1538512a1166e29f88f2607ced
Ana <anageguchadze22@gmail.com>
2025-07-03 15:34:32 +0200

    run first test to open TBC login page

--- src/test/java/LogIn.java
+++ src/test/java/LogIn.java
@@ -1,8 +1,14 @@
+import com.codeborne.selenide.Selenide;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class LogIn {
     @Test
     public void logInWithIncorrectData(){
-
+        WebDriverManager.chromedriver().setup();
+        Selenide.open("https://tbconline.ge/tbcrd/login");
+        sleep(5000);
     }
 }
